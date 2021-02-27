dc5dc89a9ed0ba73f9a924749a29eb7da1909534
Kacper Tomczak <71497968+kvcper@users.noreply.github.com>
2021-02-27 09:45:36 +0000

    Delete czilerkaBOT directory

--- czilerkaBOT/handlers/pages.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    pages(arr, itemsPerPage, page) {
-        const maxPages = Math.ceil(arr.length / itemsPerPage)
-        if(page < 1 || page > maxPages) return null;
-        return arr.slice((page - 1)* itemsPerPage, page * itemsPerPage)
-    }
-}
